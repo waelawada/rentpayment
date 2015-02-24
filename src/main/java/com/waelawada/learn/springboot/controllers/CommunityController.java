01e361487490a3dda3eeea2bdf99e722678e3d4b
Wael Awada <waelawada@gmail.com>
2015-02-23 23:14:28 -0500

    Added some methods to the community controller

--- src/main/java/com/waelawada/learn/springboot/controllers/CommunityController.java
+++ src/main/java/com/waelawada/learn/springboot/controllers/CommunityController.java
@@ -1,10 +1,14 @@
 package com.waelawada.learn.springboot.controllers;
 
 import com.waelawada.learn.springboot.converters.CommunityConverter;
+import com.waelawada.learn.springboot.domain.community.Apartment;
 import com.waelawada.learn.springboot.domain.community.Community;
+import com.waelawada.learn.springboot.domain.users.ManagerUser;
 import com.waelawada.learn.springboot.dto.community.CommunityDto;
 import com.waelawada.learn.springboot.dto.community.FullCommunityDto;
+import com.waelawada.learn.springboot.services.ApartmentService;
 import com.waelawada.learn.springboot.services.CommunityService;
+import com.waelawada.learn.springboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +25,12 @@ public class CommunityController {
     @Autowired
     private CommunityService communityService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ApartmentService apartmentService;
+
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public Community addCommunity(Community community){
         return communityService.save(community);
@@ -36,4 +46,58 @@ public class CommunityController {
         return CommunityConverter.convertEntityListToDtoList(communityService.findAll(), FullCommunityDto.class);
     }
 
+    @RequestMapping(value = "{id}/managers/", method = RequestMethod.POST)
+    public Community addManagerToCommunity(@PathVariable Long id, ManagerUser managerUser){
+        Community community = communityService.findById(id);
+        if(managerUser!=null && managerUser.getId()>0){
+            managerUser = (ManagerUser)userService.findById(managerUser.getId());
+        }
+        else {
+            managerUser = (ManagerUser) userService.save(managerUser);
+        }
+        community.getManagers().add(managerUser);
+        return communityService.save(community);
+    }
+
+    @RequestMapping(value = "{id}/managers/", method = RequestMethod.DELETE)
+    public Community removeManagerFromCommunity(@PathVariable Long id, @RequestParam Long managerId){
+        ManagerUser managerUser = (ManagerUser) userService.findById(managerId);
+        Community community = communityService.findById(id);
+        community.getManagers().remove(managerUser);
+        community = communityService.save(community);
+        return community;
+    }
+
+    @RequestMapping(value = "{id}/managers/", method = RequestMethod.GET)
+    public List<ManagerUser> getAllManagersForACommunity(@PathVariable Long id){
+        Community community = communityService.findById(id);
+        return community.getManagers();
+    }
+
+    @RequestMapping(value = "{id}/apartments/", method = RequestMethod.POST)
+    public Community addApartmentToCommunity(@PathVariable Long communityId, Apartment apartment){
+        if(apartment!=null && apartment.getId()>0){
+            apartment = apartmentService.findById(apartment.getId());
+        }
+        else{
+            apartment = apartmentService.save(apartment);
+        }
+        Community community = communityService.findById(communityId);
+        community.getApartments().add(apartment);
+        return communityService.save(community);
+    }
+
+    @RequestMapping(value = "{id}/apartments/", method = RequestMethod.DELETE)
+    public Community removeApartmentFromCommunity(@PathVariable Long id, @RequestParam Long apartmentId){
+        Apartment apartment = apartmentService.findById(apartmentId);
+        Community community = communityService.findById(id);
+        community.getApartments().remove(apartment);
+        return communityService.save(community);
+    }
+
+    @RequestMapping(value = "{id}/apartments/", method = RequestMethod.GET)
+    public List<Apartment> getAllApartmentForACommunity(@PathVariable Long id){
+        Community community = communityService.findById(id);
+        return community.getApartments();
+    }
 }
